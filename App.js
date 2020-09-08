fb527616a252f3345028b350f2691f6d63e3c195
morteza-jamali <mortezajamali4241@gmail.com>
2020-09-08 12:47:48 -0700

    React Native Elements library added !

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import React, { useEffect } from "react";
 import { Text, View, BackHandler } from "react-native";
-import Terminal from './src/Terminal';
-
-Terminal.runCommand('pwd');
+import { Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const App = () => {
   useEffect(() => {
@@ -17,8 +16,109 @@ const App = () => {
   }, []);
 
   return (
-      <View>
-        <Text>Click Back button!</Text>
+      <View style={{
+          flex: 1 ,
+          flexDirection: 'row' ,
+          position: 'absolute' ,
+          bottom: 0 ,
+          right: 0 ,
+          left: 0 ,
+          backgroundColor: 'blue'
+      }}>
+          <View style={{
+              flex: 1 ,
+              flexDirection: 'row' ,
+              justifyContent: 'space-around' ,
+              paddingTop: 23
+          }}>
+              <Button
+                  buttonStyle={{
+                      width: 50 ,
+                      height: 50 ,
+                      borderRadius: 50 ,
+                      backgroundColor: 'orange'
+                  }}
+                  icon={
+                      <Icon
+                          name="phone"
+                          size={20}
+                          color="white"
+                      />
+                  }
+              />
+              <Button
+                  buttonStyle={{
+                      width: 50 ,
+                      height: 50 ,
+                      borderRadius: 50 ,
+                      backgroundColor: 'orange'
+                  }}
+                  icon={
+                      <Icon
+                          name="phone"
+                          size={20}
+                          color="white"
+                      />
+                  }
+              />
+          </View>
+          <View style={{
+              justifyContent: 'space-around'
+          }}>
+              <Button
+                  buttonStyle={{
+                      width: 60 ,
+                      height: 60 ,
+                      borderRadius: 50 ,
+                      backgroundColor: 'green' ,
+                      marginBottom: 23
+                  }}
+                  icon={
+                      <Icon
+                          name="phone"
+                          size={20}
+                          color="white"
+                      />
+                  }
+              />
+          </View>
+          <View style={{
+              flex: 1 ,
+              flexDirection: 'row' ,
+              justifyContent: 'space-around' ,
+              paddingTop: 23
+          }}>
+              <Button
+                  buttonStyle={{
+                      width: 50 ,
+                      height: 50 ,
+                      borderRadius: 50 ,
+                      backgroundColor: 'red'
+                  }}
+                  icon={
+                      <Icon
+                          name="phone"
+                          size={20}
+                          color="white"
+                      />
+                  }
+              />
+              <Button
+                  buttonStyle={{
+                      width: 50 ,
+                      height: 50 ,
+                      borderRadius: 50 ,
+                      backgroundColor: 'red'
+                  }}
+                  icon={
+                      <Icon
+                          name="phone"
+                          size={20}
+                          color="white"
+                      />
+                  }
+              />
+          </View>
       </View>
   );
 };
